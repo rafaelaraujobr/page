e20a7a64346a905be2085de1a3bc9ddf3c260c13
Rafael Araujo <rflaraujodev@gmail.com>
2022-06-20 20:34:13 -0300

    deploy,sh

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:rafaelaraujobr/rafaelaraujobr.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rafaelaraujobr/page.git main:gh-pages
+#git push -f git@github.com:rafaelaraujobr/page.git main:gh-pages
 
 cd -
