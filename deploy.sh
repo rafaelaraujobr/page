acf3854bad5948123252da6a2142de21fd85d422
Rafael Araujo <rflaraujodev@gmail.com>
2022-06-21 15:34:23 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,21 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
-git init
-git add -A
+# git init
+git add dist
 git commit -m 'deploy'
-
+git subtree push --prefix dist origin gh-pages
+#git remote update 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:rafaelaraujobr/rafaelaraujobr.github.io.git main:gh-pages
+#git remote add origin 
+#git push -f git@github.com:rafaelaraujobr/rafaelaraujobr.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git push -f git@github.com:rafaelaraujobr/page.git main:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
